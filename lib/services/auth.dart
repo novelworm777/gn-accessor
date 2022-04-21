faaacb96cd621464d84406322debaf00591cb442
novelworm <mikhaya.jrh@gmail.com>
2022-04-21 21:10:27 +0700

    use const collection name

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gn_accessor/services/user.dart';
 
 class Auth {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Future<DocumentSnapshot> login({required String uid}) async {
-    return await _db.collection("users").doc(uid).get();
+    return await _db.collection(User.collectionName).doc(uid).get();
   }
 }
