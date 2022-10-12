a9b46aeecc596a06a1295f6ca0571e37c2c34cb2
novelworm <mikhaya.jrh@gmail.com>
2022-10-12 19:20:46 +0700

    update flutter project

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 The technologies in this application are as the following.
 
-| Tech Stack |  |
-| :---: | :---: |
-| Framework | Flutter 2.5.2 |
-| Programming Language | Dart 2.14.3 |
-| Database | Firestore |
+| Tech Stack |                 |
+| :---: |:---------------:|
+| Framework | Flutter 2.10.13 |
+| Programming Language |   Dart 2.16.2   |
+| Database |    Firestore    |
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(const GNAccessor());
+}
+
+class GNAccessor extends StatelessWidget {
+  const GNAccessor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GN Accessor',
+      home: Container(),
+    );
+  }
 }
