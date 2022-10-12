d323be7991d9c4539cd0eb57858f6fda3d326056
novelworm <mikhaya.jrh@gmail.com>
2022-10-12 20:23:57 +0700

    disable status bar and navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gn_accessor/config/route/app_router.dart';
 
 import 'config/route/routes.dart';
 
 void main() {
+  // disable status bar and navigation bar on mobile phone
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+
+  // run the app
   runApp(const GNAccessor());
 }
 
