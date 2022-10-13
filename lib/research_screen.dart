48af6ccc815ac11bdcdebccc4d2abb269db7cdcd
novelworm <mikhaya.jrh@gmail.com>
2022-10-13 18:09:18 +0700

    add basic text field

--- lib/research_screen.dart
+++ lib/research_screen.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:gn_accessor/text_field_screen.dart';
+
+const _kFontStyle = TextStyle(
+  fontSize: 21,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
 
 class ResearchScreen extends StatelessWidget {
   ResearchScreen({Key? key}) : super(key: key);
@@ -8,14 +16,57 @@ class ResearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: FormBuilder(
-          key: _formKey,
-          child: Container(),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: Container(
+          color: Colors.red.shade200,
+          padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 33),
+          child: Center(
+            child: FormBuilder(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _Label(
+                      title: 'Text Field',
+                      detailScreen: TextFieldScreen(),
+                    ),
+                    FormBuilderTextField(name: 'textField'),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _Label extends StatelessWidget {
+  const _Label({
+    Key? key,
+    required this.title,
+    required this.detailScreen,
+  }) : super(key: key);
+
+  final String title;
+  final Widget detailScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title, style: _kFontStyle),
+        TextButton(
+            onPressed: () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => detailScreen)),
+            child: const Text('Details')),
+      ],
     );
   }
 }
