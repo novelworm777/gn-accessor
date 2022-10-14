88ac9be1c1306d5a9e0c9f9009e9accbcdb1767e
novelworm <mikhaya.jrh@gmail.com>
2022-10-14 21:34:37 +0700

    create a class containing colours will be used in app

---
+++ lib/config/themes/colours.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+/// Colour theme for the app.
+class Colours {
+  /// Lighter base colour.
+  static const Color lightBase = Color(0xFF716F86);
+
+  /// Normal base colour.
+  static const Color base = Color(0xFF3E3C52);
+
+  /// Darker base colour.
+  static const Color darkBase = Color(0xFF1C1C2F);
+
+  /// Light text colour.
+  static const Color text = Color(0xFFF9F7FF);
+
+  /// Dark text colour.
+  static const Color darkText = Color(0xFF9997B0);
+
+  /// Default red colour.
+  static const Color red = Color(0xFFCE3133);
+}
