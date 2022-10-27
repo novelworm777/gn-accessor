97776cf67679353c9e36cd52deb28131594e1b03
Mikhaya Josheba Rumondang Hutagalung <7thfamiliar@gmail.com>
2022-10-27 16:57:39 +0700

    [Housekeeping] Prepare Project Structure (#3)
    * update flutter project
    
    * create a class containing screen routes
    
    * create a class to generate routes
    
    * set initial route and routes
    
    * create a class containing colours will be used in app
    
    * create a file for API endpoints
    
    * create a file for public constants
    
    * create a file for image assets' path
    
    * create a file for font assets' path
    
    * create screen size helper

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:gn_accessor/config/route/app_router.dart';
+
+import 'config/route/routes.dart';
 
 void main() {
-  runApp(Container());
+  runApp(const GNAccessor());
+}
+
+class GNAccessor extends StatelessWidget {
+  const GNAccessor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GN Accessor',
+      initialRoute: Routes.initial,
+      onGenerateRoute: AppRouter.generateRoute,
+    );
+  }
 }
