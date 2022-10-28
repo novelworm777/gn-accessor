d4ba300c18571f183f467d5fa32d32f5bef5e0e6
novelworm <mikhaya.jrh@gmail.com>
2022-10-28 19:40:38 +0700

    V2-1: create flying icon button atom

---
+++ lib/components/atoms/flying_icon_button.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../../config/themes/colours.dart';
+
+enum FlyingLocation { left, center, right }
+
+/// Icon button to be placed in header.
+class FlyingIconButton extends StatelessWidget {
+  const FlyingIconButton({
+    Key? key,
+    this.colour = Colours.darkBase,
+    this.iconColour = Colours.text,
+    required this.iconData,
+    required this.location,
+    required this.onPress,
+    this.radius = 21.0,
+  }) : super(key: key);
+
+  final Color colour;
+  final Color iconColour;
+  final IconData iconData;
+  final FlyingLocation location;
+  final VoidCallback onPress;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        child: Icon(
+          iconData,
+          color: iconColour,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: getBorderRadius(),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.75),
+              blurRadius: 4,
+              offset: const Offset(0, 4),
+            ),
+          ],
+          color: colour,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 17.0, vertical: 7.0),
+      ),
+      onTap: onPress,
+    );
+  }
+
+  BorderRadiusGeometry? getBorderRadius() {
+    switch (location) {
+      case FlyingLocation.left:
+        return BorderRadius.only(
+          bottomRight: Radius.circular(radius),
+          topRight: Radius.circular(radius),
+        );
+      case FlyingLocation.center:
+        return BorderRadius.only(
+          bottomLeft: Radius.circular(radius),
+          bottomRight: Radius.circular(radius),
+          topLeft: Radius.circular(radius),
+          topRight: Radius.circular(radius),
+        );
+      case FlyingLocation.right:
+        return BorderRadius.only(
+          bottomLeft: Radius.circular(radius),
+          topLeft: Radius.circular(radius),
+        );
+      default:
+        return null;
+    }
+  }
+}
