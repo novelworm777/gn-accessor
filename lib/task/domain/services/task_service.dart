240bff4f50422317a82c205b55482a102603dc29
novelworm <mikhaya.jrh@gmail.com>
2022-10-30 16:36:31 +0700

    T0-1: create a method to find all tasks by user id in service

--- lib/task/domain/services/task_service.dart
+++ lib/task/domain/services/task_service.dart
@@ -1,6 +1,18 @@
+import 'package:gn_accessor/task/domain/models/task.dart';
+
 import '../../data/task_repository.dart';
 
 /// Service for task module.
 class TaskService {
   final TaskRepository _repository = TaskRepository();
+
+  /// Find all tasks that a user has.
+  Future<Iterable<Task>> findAllByUserId(
+      {required String userId, bool throwError = true}) async {
+    Iterable<Task> tasks = await _repository.findTasks(userId: userId);
+    if (tasks.isEmpty && throwError) {
+      throw const FormatException("there isn't a single task");
+    }
+    return tasks;
+  }
 }
