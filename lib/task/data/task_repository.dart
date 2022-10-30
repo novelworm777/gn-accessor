d4aa0260aea5838a71936b6df6b7f804bb971348
novelworm <mikhaya.jrh@gmail.com>
2022-10-30 16:26:43 +0700

    T0-1: create find tasks method in repository

--- lib/task/data/task_repository.dart
+++ lib/task/data/task_repository.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../constants/database_collection.dart';
+import '../domain/models/task.dart';
 
 class TaskRepository {
+  /// Find all tasks of a user.
+  Future<Iterable<Task>> findTasks({required String userId}) async {
+    QuerySnapshot<Map<String, dynamic>> found = await _db(userId).get();
+    return found.docs.map((doc) => Task.create(doc.id, doc.data()));
+  }
+
   /// Create [FirebaseFirestore] instance.
   CollectionReference<Map<String, dynamic>> _db(String userId) =>
       FirebaseFirestore.instance
