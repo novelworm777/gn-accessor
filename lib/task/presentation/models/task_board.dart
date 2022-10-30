07c36dba85f4a194874db03b067e912b00b54779
novelworm <mikhaya.jrh@gmail.com>
2022-10-30 20:04:46 +0700

    T0-1: create a method to set tasks from an iterable map

--- lib/task/presentation/models/task_board.dart
+++ lib/task/presentation/models/task_board.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-import '../../domain/models/task.dart';
+import 'task.dart';
 
 /// Task board presentation model.
 class TaskBoard with ChangeNotifier, DiagnosticableTreeMixin {
@@ -9,6 +9,11 @@ class TaskBoard with ChangeNotifier, DiagnosticableTreeMixin {
 
   /// No-args constructor for [TaskBoard].
   TaskBoard();
+
+  /// Set [tasks] from a map iterable.
+  set tasks(Iterable<Map<String, dynamic>> mapIterable) {
+    _tasks = mapIterable.map<Task>((map) => Task.create(map)) as List<Task>;
+  }
 
   /// Makes [TaskBoard] readable inside devtools.
   @override
