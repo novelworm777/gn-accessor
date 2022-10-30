e9f7dd11729e7ba88c4ffa273de1e34f4def34a8
novelworm <mikhaya.jrh@gmail.com>
2022-10-30 16:47:00 +0700

    T0-1: create a method to view all tasks in usecase

--- lib/task/domain/usecases/task_usecase.dart
+++ lib/task/domain/usecases/task_usecase.dart
@@ -1,6 +1,22 @@
+import '../models/task.dart';
 import '../services/task_service.dart';
 
 /// Usecase for task module.
 class TaskUsecase {
   final TaskService _taskService = TaskService();
+
+  /// Get all tasks for task board.
+  Future<Iterable<Map<String, dynamic>>> viewAllTasks(
+      {required String userId}) async {
+    Iterable<Task> tasks = await _taskService.findAllByUserId(
+      userId: userId,
+      throwError: false,
+    );
+    return tasks.map<Map<String, dynamic>>((task) => {
+          'id': task.id,
+          'title': task.title,
+          'completed': task.completed,
+          'reward': task.reward,
+        });
+  }
 }
