7afc73cf17305cbc1173bb8ec410bbfd9fd21b6c
Mikhaya Josheba Rumondang Hutagalung <7thfamiliar@gmail.com>
2022-10-30 14:59:11 +0700

    [Feature] G0-1: Login Automatically (#12)
    * add config file
    
    * add firebase & firestore sdk
    
    * initialize firebase
    
    * G0-1: create a file for database collection names
    
    * G0-1: add user collection name
    
    * G0-1: create user domain model
    
    * G0-1: add provider sdk
    
    * G0-1: create user presentation model
    
    * G0-1: initialize provider with user model
    
    * G0-1: create user repository
    
    * G0-1: create user service
    
    * G0-1: create user usecase
    
    * G0-1: login

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:provider/provider.dart';
 
+import 'auth/presentation/models/user.dart';
 import 'config/route/app_router.dart';
 import 'config/route/routes.dart';
 
-void main() {
+void main() async {
+  // initialize firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   // disable status bar and navigation bar on mobile phone
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
 
   // run the app
-  runApp(const GNAccessor());
+  runApp(
+    MultiProvider(
+      child: const GNAccessor(),
+      providers: [
+        ChangeNotifierProvider(create: (_) => User()),
+      ],
+    ),
+  );
 }
 
 class GNAccessor extends StatelessWidget {
