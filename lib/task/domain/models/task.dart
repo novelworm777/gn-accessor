c5dd629badd3a0f8d1e13bd307b6e4b283a5b24d
novelworm <mikhaya.jrh@gmail.com>
2022-10-30 16:00:10 +0700

    T0-1: create task domain model

---
+++ lib/task/domain/models/task.dart
@@ -0,0 +1,20 @@
+class Task {
+  String? id;
+  String? title;
+  String? notes;
+  DateTime? due;
+  int? available;
+  int? completed;
+  int? reward;
+  DateTime? createdAt;
+
+  Task.create(String this.id, Map<String, dynamic> map) {
+    title = map['title'];
+    notes = map['notes'];
+    due = map['due'];
+    available = map['available'];
+    completed = map['completed'];
+    reward = map['reward'];
+    createdAt = map['created_at'];
+  }
+}
