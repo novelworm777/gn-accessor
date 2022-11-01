5b1f7d34ba1fe3ecf9e51df8a23dd0f44155bd3f
novelworm <mikhaya.jrh@gmail.com>
2022-11-01 19:20:10 +0700

    housekeeping: change _db to _firestore

--- lib/auth/data/user_repository.dart
+++ lib/auth/data/user_repository.dart
@@ -5,7 +5,8 @@ import '../domain/models/user.dart';
 
 class UserRepository {
   Future<User?> findUser(String docId) async {
-    DocumentSnapshot<Map<String, dynamic>> doc = await _db().doc(docId).get();
+    DocumentSnapshot<Map<String, dynamic>> doc =
+        await _firestore().doc(docId).get();
     if (doc.exists) return User.create(doc.id, doc.data()!);
     return null;
   }
@@ -15,9 +16,9 @@ class UserRepository {
   /// Only the data given will be updated, others will be untouched.
   void updateOne(
           {required String userId, required Map<String, dynamic> data}) =>
-      _db(userId: userId).update(data);
+      _firestore(userId: userId).update(data);
 
-  _db({String? userId}) {
+  _firestore({String? userId}) {
     if (userId != null) {
       return FirebaseFirestore.instance.collection(dUser).doc(userId);
     }
--- lib/task/data/task_repository.dart
+++ lib/task/data/task_repository.dart
@@ -6,7 +6,8 @@ import '../domain/models/task.dart';
 class TaskRepository {
   /// Find all tasks of a user.
   Future<Iterable<Task>> findTasks({required String userId}) async {
-    QuerySnapshot<Map<String, dynamic>> found = await _db(userId: userId).get();
+    QuerySnapshot<Map<String, dynamic>> found =
+        await _firestore(userId: userId).get();
     return found.docs.map((doc) => Task.create(doc.id, doc.data()));
   }
 
@@ -14,7 +15,7 @@ class TaskRepository {
   Future<Task?> findOne(
       {required String userId, required String taskId}) async {
     DocumentSnapshot<Map<String, dynamic>> doc =
-        await _db(userId: userId, taskId: taskId).get();
+        await _firestore(userId: userId, taskId: taskId).get();
     if (doc.exists) return Task.create(doc.id, doc.data()!);
     return null;
   }
@@ -27,11 +28,11 @@ class TaskRepository {
     required String taskId,
     required Map<String, dynamic> data,
   }) {
-    _db(userId: userId, taskId: taskId).update(data);
+    _firestore(userId: userId, taskId: taskId).update(data);
   }
 
   /// Create [FirebaseFirestore] instance.
-  _db({required String userId, String? taskId}) {
+  _firestore({required String userId, String? taskId}) {
     if (taskId != null) {
       return FirebaseFirestore.instance
           .collection(dUser)
