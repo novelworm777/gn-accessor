5adb8cff6243ded7e5cdac5b22d9481126f9e223
novelworm <mikhaya.jrh@gmail.com>
2022-11-03 22:10:35 +0700

    bugfix: updating nullable prop args

--- lib/domain/models/task.dart
+++ lib/domain/models/task.dart
@@ -1,6 +1,6 @@
 /// Task domain model.
 class Task {
-  String? id;
+  String id;
   String? title;
   String? notes;
   DateTime? due;
@@ -10,7 +10,7 @@ class Task {
   DateTime? createdAt;
 
   /// Creates [Task] from an id and a map.
-  Task.create(String this.id, Map<String, dynamic> map) {
+  Task.create(this.id, Map<String, dynamic> map) {
     title = map['title'];
     notes = map['notes'];
     due = map['due']?.toDate();
