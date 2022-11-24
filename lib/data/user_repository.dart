df4e50ace5b18afe9997f0c24c318326594769a4
novelworm <mikhaya.jrh@gmail.com>
2022-11-24 20:33:03 +0700

    housekeeping: use converter with user data model

--- lib/data/user_repository.dart
+++ lib/data/user_repository.dart
@@ -1,42 +1,53 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import '../domain/models/user.dart';
+import '../domain/models/user_domain.dart';
 import '../utils/services/firestore.dart';
+import 'models/user_firestore_data.dart';
 
 /// Repository for user [FirebaseFirestore] collection.
 class UserRepository {
   final _firestore = Firestore.user();
 
   /// Find all user data.
-  Future<List<User>> findAllWhereEqualTo({
+  Future<List<UserDomain>> findAllWhereEqualTo({
     required String field,
     required dynamic value,
   }) async {
-    QuerySnapshot<Map<String, dynamic>> found =
+    final QuerySnapshot<UserFirestoreData> found =
         await _users().where(field, isEqualTo: value).get();
     return found.docs
-        .map<User>((doc) => User.create(doc.id, doc.data()))
+        .map<UserDomain>((snapshot) => UserDomain.fromData(snapshot.data()))
         .toList();
   }
 
   /// Find a user data
-  Future<User?> findOne({required String userId}) async {
-    DocumentSnapshot<Map<String, dynamic>> doc =
-        await _users().doc(userId).get();
-    if (doc.exists) return User.create(doc.id, doc.data()!);
+  Future<UserDomain?> findOne({required String userId}) async {
+    final UserFirestoreData? found = await _users().doc(userId).get().then(
+          (snapshot) => snapshot.data(),
+          onError: (error) => null,
+        );
+    if (found != null) return UserDomain.fromData(found);
     return null;
   }
 
   /// Update a user data.
   ///
   /// Only the data given will be updated, others will be untouched.
-  void updateOne({
+  Future<UserDomain?> updateOne({
     required String userId,
-    required Map<String, dynamic> data,
-  }) {
-    _users().doc(userId).update(data);
+    required UserDomain data,
+  }) async {
+    await _users()
+        .doc(userId)
+        .set(UserFirestoreData.fromDomain(data), SetOptions(merge: true));
+    return findOne(userId: userId);
   }
 
   /// Create [FirebaseFirestore] instance for user collection.
-  CollectionReference<Map<String, dynamic>> _users() => _firestore;
+  CollectionReference<UserFirestoreData> _users() =>
+      _firestore.withConverter<UserFirestoreData>(
+        fromFirestore: (snapshot, _) =>
+            UserFirestoreData.fromFirestore(snapshot),
+        toFirestore: (model, _) => model.toFirestore(),
+      );
 }
