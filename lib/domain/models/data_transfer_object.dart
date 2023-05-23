5ae7e210a1f1ad42b1ebb4fa0502254705b5cf6a
novelworm <mikhaya.jrh@gmail.com>
2023-05-23 22:28:31 +0700

    D-7: create dto for where

--- lib/domain/models/data_transfer_object.dart
+++ lib/domain/models/data_transfer_object.dart
@@ -1,10 +1,21 @@
 /// DTO to store order by.
 class OrderBy {
   final String field;
-  final bool byAscending;
+  final bool isAscending;
 
   const OrderBy({
     this.field = 'id',
-    this.byAscending = true,
+    this.isAscending = true,
+  });
+}
+
+/// DTO to store where.
+class Where {
+  final String field;
+  final dynamic value;
+
+  const Where({
+    required this.field,
+    required this.value,
   });
 }
